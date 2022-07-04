b35f9bd27c10a07c4111d0d8f3cd90f2d4e9f6c1
ahnniething <ahnniething@gmail.com>
2022-07-04 11:47:02 +0900

    #5 feat: initialize the offset when touch finished and close #5

--- App.js
+++ App.js
@@ -48,6 +48,7 @@ export default function App() {
       },
       onPanResponderRelease: () => {
         console.log("Touch Finished");
+        position.flattenOffset();
       },
     })
   ).current;
